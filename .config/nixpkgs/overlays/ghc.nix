3a0207d0440e23b8253ad4f524a106e2a89be002
Jordan Mackie <jmackie@protonmail.com>
2018-10-10 12:31:39 +0100

    Formatting

--- .config/nixpkgs/overlays/ghc.nix
+++ .config/nixpkgs/overlays/ghc.nix
@@ -6,7 +6,7 @@ self: super:
         hpack
     ]);
 
-    # Older stack projects need this attribute to be present in order to build
-    # with Nix.
-    haskell.compiler.ghc821 = super.haskell.compiler.ghc821Binary;
+    # Older stack projects need this attribute to be present
+    # in order to build with Nix.
+    #haskell.compiler.ghc821 = super.haskell.compiler.ghc821Binary;
 }
